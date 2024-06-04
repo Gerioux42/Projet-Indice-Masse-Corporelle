b49e8c3bda238575267b915203a7b81cc3c86bdd
Gerioux42 <michelramamonjison@gmail.com>
2024-06-04 18:49:27 +0300

    Update Page1.js

--- screens/Page1.js
+++ screens/Page1.js
@@ -9,71 +9,71 @@ const Page1 = ({ navigation }) => {
 
     return () => clearTimeout(timer);
   }, [navigation]);
-const [currentPage, setCurrentPage] = React.useState(1); // Page 3 est la page actuelle par défaut
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title1}>Bienvenue sur</Text>
-      <Image source={require('../assets/IMC Logo.png')} style={styles.logo} />
-      <Text style={styles.title2}>Indice de Masse Corporelle</Text>
-        <View style={styles.pagination}>
-        <View style={[styles.pageIndicator, currentPage === 1 && styles.pageIndicatorFilled]} />
-        <View style={[styles.pageIndicator, currentPage === 2 && styles.pageIndicatorFilled]} />
-        <View style={[styles.pageIndicator, currentPage === 3 && styles.pageIndicatorFilled]} />
-        <View style={[styles.pageIndicator, currentPage === 4 && styles.pageIndicatorFilled]} />
+const [currentPage, setCurrentPage] = Réagir.useState(1); 
+  retour (
+    <View style={styles.récipient}>
+      <Text style={styles. .title1}> Bienvenue sur </Text>
+      <Image source={exiger('../assets / IMC Logo.png ')} style ={styles.logo} />
+      <Text style={styles. .title2}> Indice de Masse Corporelle </Text>
+        <View style={styles. .pagination}>
+        <View style={[styles.pageIndicateur, currentPage === 1 && styles.pageIndicatorFilled]} />
+        <View style={[styles.pageIndicateur, currentPage === 2 && styles.pageIndicatorFilled]} />
+        <View style={[styles.pageIndicateur, currentPage === 3 && styles.pageIndicatorFilled]} />
+        <View style={[styles.pageIndicateur, currentPage === 4 && styles.pageIndicatorFilled]} />
       </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
+const styles = StyleSheet.créer({
+  récipient: {
     flex: 1,
-    backgroundColor: '#005858',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '# 005858',
+    alignItems: 'centre',
+    justifyContent: 'centre',
   },
-  title1: {
+  titre 1: {
     fontSize: 32,
-    fontFamily: 'Regular',
-    color: '#FFFFFF',
-    position: 'absolute',
-    top: 192,
+    fontFamily: 'Régulier',
+    couleur: '#FFFFFF',
+    position: 'absolu',
+    Haut: 192,
   },
   title2: {
     fontSize: 24,
-    fontFamily: 'Regular',
-    color: '#FFFFFF',
-    position: 'absolute',
-    bottom: 100,
+    fontFamily: 'Régulier',
+    couleur: '#FFFFFF',
+    position: 'absolu',
+    bas: 100,
   },
   logo: {
-    width: 268,
-    height: 276,
-    position: 'absolute',
-    top: 273,
+    largeur: 268,
+    hauteur: 276,
+    position: 'absolu',
+    Haut: 273,
   },
 
 pagination: {
-  position: 'absolute',
-  bottom: 20, // Positionnement à 0.5 cm du bas
-  left: 0,
-  right: 0,
-  flexDirection: 'row',
-  justifyContent: 'center',
-  alignItems: 'center',
-  zIndex: 1, // Assurez-vous que la pagination se trouve au-dessus du contenu de la page
-  // marginBottom: -10, // Pour compenser l'espace ajouté par la position absolue
+  position: 'absolu',
+  bas: 20,
+  gauche: 0,
+  droite: 0,
+  flexDirection: 'ligne',
+  justifyContent: 'centre',
+  alignItems: 'centre',
+  zIndex: 1, 
+  // marginBottom: -10, 
 },
-  pageIndicator: {
-    width: 10,
-    height: 10,
+  pageIndicateur: {
+    largeur: dix,
+    hauteur: dix,
     borderRadius: 5,
     backgroundColor: '#FFFFFF',
     marginHorizontal: 5,
   },
   pageIndicatorFilled: {
-    backgroundColor: '#00FEDF',
+    backgroundColor: '# 00FEDF',
   },
 });
 
-export default Page1;
+exportation par défaut Page1;
